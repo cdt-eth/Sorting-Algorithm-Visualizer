e2be5e031d90e09797a5a189f9bb8c7bc9e1cfd0
christianturner <christiandavidphoto@gmail.com>
2021-02-02 09:32:31 -0500

    create all buttons

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -17,8 +17,8 @@ export default class SortingVisualizer extends React.Component {
 
   resetArray() {
     const array = [];
-    for (let i = 0; i < 100; i++) {
-      array.push(randomIntFromInterval(5, 730));
+    for (let i = 0; i < 275; i++) {
+      array.push(randomIntFromInterval(5, 630));
     }
     this.setState({ array });
   }
@@ -27,17 +27,25 @@ export default class SortingVisualizer extends React.Component {
     const { array } = this.state;
 
     return (
-      <>
+      <div className="array-container">
         {array.map((value, idx) => (
-          <div calssName="array-bar" key={idx}>
-            {value}
-          </div>
+          <div
+            className="array-bar"
+            key={idx}
+            style={{ height: `${value}px` }}
+          ></div>
         ))}
-      </>
+        <br/>
+        <button onClick={() => this.resetArray()}>New Array</button>
+        <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+        <button onClick={() => this.quickSort()}>Quicksort</button>
+        <button onClick={() => this.mergeSort()}>Merge Sort</button>
+        <button onClick={() => this.heapSort()}>Heap Sort</button>
+      </div>
     );
   }
 }
 
-function randomIntFromInterval(min, max){
-    return Math.floor(Math.random()*(max-min + 1) + min)
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
